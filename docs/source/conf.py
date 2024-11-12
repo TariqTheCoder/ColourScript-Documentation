a309ebf552fc37fd739d6182a5b84e5c9f4ec482
TariqTheCoder <tariqtayebi1@outlook.com>
2024-11-12 21:27:07 +1100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ColourScript'
+copyright = '2024, Tariq Tayebi'
+author = 'Tariq Tayebi'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.0.5'
 
 # -- General configuration
 
